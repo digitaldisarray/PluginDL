f3a9d04992f483d55e1fb65c3241310eba6295df
Kevin <electrichexagon8@gmail.com>
2021-05-23 01:25:34 -0700

    JSON file saving and avoiding repeats

--- src/xyz/disarray/Plugin.java
+++ src/xyz/disarray/Plugin.java
@@ -1,5 +1,7 @@
 package xyz.disarray;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,66 +9,124 @@ import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class Plugin extends Thread {
+
+	// Info & statistics about the plugin
 	private String url;
 	private String name;
 	private int id;
 	private String created;
 	private String updated;
 	private String totalDownloads;
-	private List<String> categories;
+	private String categories;
 
+	// Json object that represents the plugin as a whole
+	private JSONObject plugin;
+
+	// List of all the releases that this plugin has
 	private List<PluginFile> downloaded;
+
+	// Is the plugin done downloading all the releases
+	private boolean done = false;
 
 	public Plugin(String url) {
 		this.url = url;
 		downloaded = new ArrayList<>();
 	}
 
+	public Plugin(String url, String name, int id, String created, String updated, String totalDownloads,
+			String categories) {
+		this.url = url;
+		this.name = name;
+		this.id = id;
+		this.created = created;
+		this.updated = updated;
+		this.totalDownloads = totalDownloads;
+		this.categories = categories;
+
+		// Load plugin files that we already downloaded from json
+		JSONParser jsonParser = new JSONParser();
+		try (FileReader reader = new FileReader(
+				System.getProperty("user.dir") + "/downloads/" + url.split("/")[4] + "/files.json")) {
+			Object obj = jsonParser.parse(reader);
+			JSONArray pluginList = (JSONArray) obj;
+
+			// Iterate over employee array
+			for (Object o : pluginList) {
+				downloaded.add(parsePluginFileObj((JSONObject) o));
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private PluginFile parsePluginFileObj(JSONObject o) {
+		String name = (String) o.get("name");
+		String fileSize = (String) o.get("fileSize");
+		String dlPageLink = (String) o.get("link");
+		String uploadDate = (String) o.get("uploadDate");
+		String gameVersions = (String) o.get("versions");
+		String fileName = (String) o.get("fileName");
+		return new PluginFile(name, fileSize, dlPageLink, uploadDate, gameVersions, fileName);
+	}
+
 	public void start() {
-		// Fetch data
-		Document d = Util.get(url);
-		
-		// NAME
-		this.name = d.select("span[class=overflow-tip]").first().text();
-		
-		// INFO/PROPERTIES
-		List<String> properties = new ArrayList<>();
-		for(Element e : d.select("div[class=info-data]")) 
-			properties.add(e.text());
-		
-		this.id = Integer.parseInt(properties.get(0));
-		this.created = properties.get(1);
-		this.updated = properties.get(2);
-		this.totalDownloads = properties.get(3);
-		
-		// CATEGORIES
-		categories = new ArrayList<>();
-		for(Element e : d.select("a[class='e-avatar32 tip']"))
-			categories.add(e.attr("title"));
-		
+
+		Document d;
+
+		// If we already have the info, no need to get this info again
+		if (name == null) {
+			// Fetch data
+			d = Util.get(url);
+			// NAME
+			this.name = d.select("span[class=overflow-tip]").first().text();
+
+			// INFO/PROPERTIES
+			List<String> properties = new ArrayList<>();
+			for (Element e : d.select("div[class=info-data]"))
+				properties.add(e.text());
+
+			this.id = Integer.parseInt(properties.get(0));
+			this.created = properties.get(1);
+			this.updated = properties.get(2);
+			this.totalDownloads = properties.get(3);
+
+			// CATEGORIES
+			categories = "";
+			for (Element e : d.select("a[class='e-avatar32 tip']"))
+				categories += e.attr("title") + " ";
+		}
+
 		// Get plugin files/versions etc
 		d = Util.get(url + "/files");
 		// Dl file page while next is available
 		Element next = d.selectFirst("a[rel=next]");
-		if(next == null) {
+		if (next == null) {
 			dlPluginsFromPage(d);
 		} else {
-			while(next != null) {
+			while (next != null) {
 				dlPluginsFromPage(d);
 				d = Util.get("https://dev.bukkit.org" + d.selectFirst("a[rel=next]").attr("href"));
 				next = d.selectFirst("a[rel=next]");
 			}
 		}
-		
+
 		// Create the json that stores the list of jar files
 		JSONArray pluginFiles = new JSONArray();
-		for(PluginFile f : downloaded) {
+		for (PluginFile f : downloaded) {
 			JSONObject file = new JSONObject();
-			
+
 			// Add all the properties of the plugin release file to an object
 			file.put("name", f.getName());
 			file.put("fileName", f.getFileName());
@@ -75,25 +135,31 @@ public class Plugin extends Thread {
 			file.put("link", f.getDownloadPageLink());
 			file.put("versions", f.getGameVersions());
 			file.put("uploadDate", f.getUploaded());
-			
+
 			// Add the object to the array of files
 			pluginFiles.add(file);
 		}
-		
+
 		// Write the json file to the plugins directory
-		String current = System.getProperty("user.dir");
-		System.out.println(pluginFiles.toJSONString());
-		try (FileWriter file = new FileWriter(System.getProperty("user.dir") + "/downloads/" + url.split("/")[4] + "/plugins.json")) {
-            file.write(pluginFiles.toJSONString().replace("\\/", "/")); 
-            file.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-		
-		// Create json object
-		JSONObject plugin = new JSONObject();
-		//plugin.put("url", url);
-		// ...
+		try (FileWriter file = new FileWriter(
+				System.getProperty("user.dir") + "/downloads/" + url.split("/")[4] + "/files.json")) {
+			file.write(pluginFiles.toJSONString().replace("\\/", "/"));
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Create json object that represents the plugin as a whole
+		plugin = new JSONObject();
+		plugin.put("name", name);
+		plugin.put("id", id);
+		plugin.put("url", url);
+		plugin.put("categories", categories);
+		plugin.put("created", created);
+		plugin.put("updated", updated);
+		plugin.put("totalDownloads", Integer.parseInt(totalDownloads.replaceAll(",", "")));
+
+		done = true;
 	}
 
 	private void dlPluginsFromPage(Document d) {
@@ -101,6 +167,17 @@ public class Plugin extends Thread {
 			Element downloadCell = entry.select("a[class='overflow-tip twitch-link']").first();
 			String name = downloadCell.text();
 			String dlPageLink = url.substring(0, url.indexOf(".org/") + 4) + downloadCell.attr("href");
+
+			// If we already have this plugin downloaded, skip it
+			boolean alreadyDownloaded = false;
+			for(PluginFile p : downloaded) {
+				if(dlPageLink.equals(p.getDownloadPageLink()));
+			}
+			if(alreadyDownloaded) {
+				System.out.println("Already downloaded " + name + "! Skipping...");
+				continue;
+			}
+
 			String fileSize = entry.select("td[class=project-file-size]").first().text();
 			String uploadDate = entry.select("abbr").first().text();
 			String gameVersions = entry.select("span[class=version-label]").first().text() + "";
@@ -114,10 +191,18 @@ public class Plugin extends Thread {
 			String fileName = Util.get(dlPageLink).selectFirst("div[class^='info-data overflow-tip']").text();
 
 			PluginFile pf = new PluginFile(name, fileSize, dlPageLink, uploadDate, gameVersions, fileName);
-			pf.download();
+			//pf.download();
 			downloaded.add(pf);
 		}
-		System.out.println("Completed downloading plugin: " + name);
+		System.out.println("Completed downloading a page for plugin: " + name);
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public JSONObject toJson() {
+		return plugin;
 	}
 
 }
--- src/xyz/disarray/PluginDL.java
+++ src/xyz/disarray/PluginDL.java
@@ -1,8 +1,16 @@
 package xyz.disarray;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -17,6 +25,26 @@ public class PluginDL extends Thread {
 	}
 
 	public void run() {
+
+		// Read the json and init plugin objects that we have previously downloaded
+		JSONParser jsonParser = new JSONParser();
+		try (FileReader reader = new FileReader(System.getProperty("user.dir") + "/downloads/plugins.json")) {
+			Object obj = jsonParser.parse(reader);
+			JSONArray pluginList = (JSONArray) obj;
+
+			// Iterate over employee array
+			for(Object o : pluginList) {
+				plugins.add(parsePluginObj((JSONObject) o));
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
 		// Get the first page
 		Document firstPage = Util.get("https://dev.bukkit.org/bukkit-plugins");
 
@@ -38,8 +66,7 @@ public class PluginDL extends Thread {
 		// DEBUG PURPOSES:
 		pages = 1;
 
-		// Loop through each page getting all of the links & create plugin objects out
-		// each one
+		// Loop through each page & create plugin objects out of every link
 		for (int i = 1; i <= pages; i++) {
 			Document doc = Util.get("https://dev.bukkit.org/bukkit-plugins?page=" + i);
 
@@ -58,8 +85,62 @@ public class PluginDL extends Thread {
 		// Start each plugin thread
 		plugins.get(0).start();
 
+		/*
+		 * for(Plugin p : plugins) { p.start(); }
+		 */
+
+		// Check if all downloads are finished
+		boolean allFinished = false;
+		while (!allFinished) {
+			
+			// debug
+			if(plugins.get(0).isDone()) {
+				allFinished = true;
+			}
+			
+			
+			//boolean foundUnfinished = false;
+			//for (Plugin p : plugins) {
+			//	if (!p.isDone()) {
+			//		foundUnfinished = true;
+			//		break;
+			//	}
+			//}
+
+			//if (!foundUnfinished) {
+			//	allFinished = true;
+			//}
+		}
+		
+		System.out.println("All plugins completed");
+		
+		// All plugins are done downloading, save the json entries to a file
+		JSONArray pluginEntries = new JSONArray();
+		for (Plugin p : plugins) {
+			pluginEntries.add(p.toJson());
+		}
+
+		try (FileWriter file = new FileWriter(System.getProperty("user.dir") + "/downloads/plugins.json")) {
+			file.write(pluginEntries.toJSONString().replace("\\/", "/"));
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
+	private Plugin parsePluginObj(JSONObject plugin) {
+		String url = (String) plugin.get("url");
+		String name = (String) plugin.get("name");
+		int id = (int) plugin.get(name);
+		String created = (String) plugin.get("created");
+		String updated = (String) plugin.get("updated");
+		String totalDownloads = (String) plugin.get("totalDownloads");
+		String categories = (String) plugin.get("categories");
+		return new Plugin(url, name, id, created, updated, totalDownloads, categories); 
+	}
+
+	
 	public void exit() {
 		running = false;
 	}
